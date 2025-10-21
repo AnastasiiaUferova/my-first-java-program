0cf51c7861e36996b88637e657bf243334d95b5f
‘Anastasiia <miss.iuferova@yandex.ru>
2025-10-21 19:00:45 +0200

    feat: for loops

--- src/main/java/anastasiia/org/Main.java
+++ src/main/java/anastasiia/org/Main.java
@@ -3,12 +3,69 @@ package anastasiia.org;
 
 import lol.Lol;
 
+import java.util.ArrayList;
+
 public class Main {
   static void main() {
 
     System.out.println("Hello world");
 
     Lol.sayLol();
+
+    int myAge = 28;
+    System.out.println(myAge);
+    var timePassed = "1 year later";
+    System.out.println(timePassed);
+    myAge++;
+    System.out.println(myAge);
+    timePassed  = "2 years later";
+    System.out.println(timePassed);
+
+    String[] cars = {"BMW", "Volvo", "Toyota"};
+
+    for(String car : cars) {
+      System.out.println(car);
+    }
+
+    int[] ages = {2,45,23,90};
+
+    for(int age : ages){
+      if(age > 25){
+        System.out.println(age);
+      }
+    }
+
+    ArrayList<String> languages = new ArrayList<>();
+
+    languages.add("Java");
+    languages.add("JavaScript");
+    languages.add("PHP");
+    languages.add("C++");
+
+    languages.addFirst("Python");
+    languages.add(1,"Kotlin");
+
+    var lang = languages.get(2);
+
+    languages.set(5, ".NET");
+
+    System.out.println(lang);
+
+    languages.remove(1);
+
+    for (String language : languages) {
+      System.out.println(language);
+    }
+
+
+    languages.clear();
+
+    System.out.println(languages);
+
+    for(int i = 10; i>=0; i--){
+      System.out.println(i);
+    }
+    
   }
 
 }
---
+++ src/test/java/anastasiia/org/MainTest.java
@@ -0,0 +1,12 @@
+package anastasiia.org;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+  @Test
+  public void myFirstTest() {
+    assertTrue(true);
+  }
+}
