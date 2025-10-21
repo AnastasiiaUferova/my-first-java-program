3173298dc5f18c67337e3b3cf421715a00ef4eb7
‘Anastasiia <miss.iuferova@yandex.ru>
2025-10-21 16:51:18 +0200

    setup: first code

---
+++ src/main/java/anastasiia/org/Main.java
@@ -0,0 +1,14 @@
+package anastasiia.org;
+
+
+import lol.Lol;
+
+public class Main {
+  static void main() {
+
+    System.out.println("Hello world");
+
+    Lol.sayLol();
+  }
+
+}
---
+++ src/main/java/lol/Lol.java
@@ -0,0 +1,9 @@
+package lol;
+
+public class Lol {
+  public static void sayLol(){
+    System.out.println("LOL");
+  }
+}
+
+
